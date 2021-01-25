d54434a3f7fd1d696265c7ade06d0a51abd3438b
mzmjsy <mzmjsy@126.com>
2021-01-25 18:16:18 +0800

    添加链接

--- src/main/java/choice/common/JdbcConnection.java
+++ src/main/java/choice/common/JdbcConnection.java
@@ -8,6 +8,9 @@ import javax.sql.DataSource;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+/**
+ * @author 马振
+ */
 public class JdbcConnection {
 	
 	public static final String ORACLE="oracle";
@@ -15,6 +18,7 @@ public class JdbcConnection {
 	public static final String MYSQL="mysql";
 	
 	private static DataSource IMS_ds;
+	private static DataSource DST_ds;
 	private static DataSource YC_ds;
 	
 	private static int minPoolSize=5;
@@ -33,7 +37,7 @@ public class JdbcConnection {
 	static {
 		try {
 			// 读取配置文件中的jdbc参数信息
-			ResourceMap ims_rm = new ResourceMap(ItemPath.getItemPathBSorCS()+"/classes/jdbc");
+			ResourceMap ims_rm = new ResourceMap(ItemPath.getItemPathBSorCS()+"/jdbc");
 			String ims_driver = ims_rm.getString("jdbc.driver_ims");
 			String ims_url = ims_rm.getString("jdbc.url_ims");
 			String ims_user = ims_rm.getString("jdbc.username_ims");
@@ -66,7 +70,41 @@ public class JdbcConnection {
 	}
 	static {
 		try {
-			ResourceMap yc_rm = new ResourceMap(ItemPath.getItemPathBSorCS()+"/classes/jdbc");
+			// 读取配置文件中的jdbc参数信息
+			ResourceMap dst_rm = new ResourceMap(ItemPath.getItemPathBSorCS()+"/jdbc");
+			String dst_driver = dst_rm.getString("jdbc.driver_dst");
+			String dst_url = dst_rm.getString("jdbc.url_dst");
+			String dst_user = dst_rm.getString("jdbc.username_dst");
+			String dst_pwd = dst_rm.getString("jdbc.password_dst");
+			/*
+			 * 加载配置文件以后，得到了数据库的 连接参数。利用连接参数初始化连接池 对象。
+			 */
+			ComboPooledDataSource dst_ds = new ComboPooledDataSource();
+			dst_ds.setDriverClass(dst_driver);
+			dst_ds.setJdbcUrl(dst_url);
+			dst_ds.setUser(dst_user);
+			dst_ds.setPassword(dst_pwd);
+			dst_ds.setMinPoolSize(minPoolSize);
+			dst_ds.setMaxPoolSize(maxPoolSize);
+			dst_ds.setInitialPoolSize(initialPoolSize);
+			dst_ds.setMaxIdleTime(maxIdleTime);
+			dst_ds.setAcquireIncrement(acquireIncrement);
+			dst_ds.setCheckoutTimeout(checkoutTimeout);
+			dst_ds.setMaxStatements(maxStatements);
+			dst_ds.setIdleConnectionTestPeriod(idleConnectionTestPeriod);
+			dst_ds.setAcquireRetryAttempts(acquireRetryAttempts);
+			dst_ds.setAcquireRetryDelay(acquireRetryDelay);
+			dst_ds.setBreakAfterAcquireFailure(breakAfterAcquireFailure);
+			dst_ds.setTestConnectionOnCheckout(testConnectionOnCheckout);
+			DST_ds = dst_ds;
+		} catch (Exception e) {
+			e.printStackTrace();
+			LogUtil.writeToTxt(LogUtil.IMSXML, "jdbc", "德顺通数据库" + e.toString());
+		}
+	}
+	static {
+		try {
+			ResourceMap yc_rm = new ResourceMap(ItemPath.getItemPathBSorCS()+"/jdbc");
 			String yc_driver = yc_rm.getString("jdbc.driver_yc");
 			String yc_url = yc_rm.getString("jdbc.url_yc");
 			String yc_user = yc_rm.getString("jdbc.username_yc");
@@ -100,7 +138,7 @@ public class JdbcConnection {
 	 * 描述:IMS数据库连接
 	 * 作者:马振
 	 * 时间:2016年8月26日上午9:32:19
-	 * @return
+	 * @return Connection
 	 */
 	public static Connection getImsConnection() {
 		Connection conn = null;
@@ -112,12 +150,29 @@ public class JdbcConnection {
 		}
 		return conn;
 	}
-	
+
+	/**
+	 * 描述:德顺通数据库连接
+	 * 作者:马振
+	 * 时间:2016年8月26日上午9:32:19
+	 * @return Connection
+	 */
+	public static Connection getDstConnection() {
+		Connection conn = null;
+		try {
+			conn = DST_ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+			LogUtil.writeToTxt(LogUtil.IMSXML, "jdbc", "德顺通库" + e.getMessage());
+		}
+		return conn;
+	}
+
 	/**
 	 * 描述:雅昌数据库连接
 	 * 作者:马振
 	 * 时间:2020年12月2613日
-	 * @return
+	 * @return Connection
 	 */
 	public static Connection getYcConnection() {
 		Connection conn = null;
@@ -156,7 +211,7 @@ public class JdbcConnection {
 	}
 	
 	public static void main(String[] args) {
-       	ResourceMap rm = new ResourceMap(ItemPath.getItemPathBSorCS() + "/classes/jdbc");
+       	ResourceMap rm = new ResourceMap(ItemPath.getItemPathBSorCS() + "/jdbc");
 		String driver = rm.getString("jdbc.driver_yc");
 		System.out.println(ItemPath.getItemPathBSorCS() + "jdbc");
 		System.out.println(driver);
